147ca93015772e0bf3c4d49b3a9d5f80cfd9e91c
James Noria <jnoria.dev@gmail.com>
2022-08-08 18:09:03 -0500

    ejemplo con el equipo

--- src/app.js
+++ src/app.js
@@ -9,4 +9,46 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(morgan('dev'));
 
+app.get('/users', (req, res) => {
+  User.findAll().then((users) => {
+    res.json(users);
+  });
+});
+
+app.post('/users', (req, res) => {
+  const { nombre, apellidos, email, password, confirmarPassword } = req.body;
+
+  User.create({
+    nombre,
+    apellidos,
+    email,
+    password,
+    confirmarPassword,
+  }).then((user) => {
+    res.status(200).json(user);
+  });
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  User.findOne({
+    where: {
+      email,
+      password,
+    },
+  }).then((user) => {
+    res.status(200).json(user.nombre);
+  });
+})
+
+
+app.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+
+  User.findByPk(id).then((user) => {
+    res.json(user);
+  });
+})
+
 export default app;
