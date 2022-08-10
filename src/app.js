5d9ad21f813399c95e3b9ef9b54ecaedefef3ac1
khuaza9612 <102488101+kevinhuaza@users.noreply.github.com>
2022-08-09 20:08:39 -0500

    login

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 import express from 'express';
 import morgan from 'morgan';
+import User from './models/UserModel.js';
+
+
+
+
+
+
 
 const app = express();
 
@@ -7,5 +14,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(morgan('dev'));
+
+const login= async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+        return res.status(404).json({ msg: 'El usuario no existe' });
+    }
+    if (user.password !== password) {
+        return res.status(404).json({ msg: 'Contraseña incorrecta' });
+    }
+    return res.json({ msg: 'Login correcto' });
+};
+app.post('/api/login', login);
 
 export default app;
