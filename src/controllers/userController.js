157688fd78af9b5156aa095977d5761e6cb8146f
James Noria <jnoria.dev@gmail.com>
2022-08-10 17:40:36 -0500

    test: deploy

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,12 +1,12 @@
-import User from '../models/userModel.js';
-import handleHttpError from '../utils/httpError.js';
+// import User from '../models/userModel.js';
+// import handleHttpError from '../utils/httpError.js';
 
-export const getUser = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const user = await User.findByPk(id);
-    res.json(user);
-  } catch (error) {
-    handleHttpError(res, error);
-  }
-};
+// export const getUser = async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const user = await User.findByPk(id);
+//     res.json(user);
+//   } catch (error) {
+//     handleHttpError(res, error);
+//   }
+// };
