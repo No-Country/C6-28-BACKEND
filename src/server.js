4301b128944b4a2a61dfafb1a8bf82e60bc11584
James Noria <jnoria.dev@gmail.com>
2022-08-11 03:45:59 -0500

    fix: bug

--- src/server.js
+++ src/server.js
@@ -2,10 +2,12 @@
 import app from './app.js';
 import { sequelize } from './database/database.js';
 
+const PORT = process.env.PORT || 3000;
+
 sequelize.sync({ force: false }).then(() => {
   console.log('✓ Se conectó a la base de datos');
 });
 
-app.listen(3000, () => {
-  console.log('✓ Servidor corriendo en el puerto 3000');
+app.listen(PORT, () => {
+  console.log('✓ Servidor corriendo en el puerto ' + PORT);
 });
