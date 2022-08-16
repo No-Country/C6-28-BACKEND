ae84b7c34b270574183de820251fb4a166f02a11
Mikel Diaz <85890651+SBolivarLoL@users.noreply.github.com>
2022-08-16 15:07:09 -0500

    feat: aut middleware to secure sensitive endpoints and new roles column in DB

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,14 +2,14 @@ import express from 'express';
 const router = express.Router();
 import * as UserRoute from '../controllers/userController.js';
 import * as AuthRoute from '../controllers/authController.js';
+import * as AuthMiddleware from '../middlewares/authMiddleware.js';
 
 router.post('/login', AuthRoute.login);
 router.post('/signup', AuthRoute.signUp);
 router.get('/logout', AuthRoute.getLogout);
 
-
-router.get('/:id', UserRoute.getUser);
-router.get('/', UserRoute.getAllUsers);
-router.delete('/:id', UserRoute.deleteUser);
+router.get('/:id', AuthMiddleware.ensureAuth, UserRoute.getUser);
+router.get('/', AuthMiddleware.ensureAuth, UserRoute.getAllUsers);
+router.delete('/:id', AuthMiddleware.ensureAuth, UserRoute.deleteUser);
 
 export default router;
