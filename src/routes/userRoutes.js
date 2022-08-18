3821ed40c57ba9297880a63ee335a58b0855b1ec
khuaza9612 <102488101+kevinhuaza@users.noreply.github.com>
2022-08-18 14:19:29 -0500

    limite de llamado

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 import * as UserRoute from '../controllers/userController.js';
 import * as AuthRoute from '../controllers/authController.js';
 import * as AuthMiddleware from '../middlewares/authMiddleware.js';
+//import * as RateLimit from '../middlewares/ratelimit.js';
 
-router.post('/login', AuthRoute.login);
+router.post('/login',AuthRoute.login);
 router.post('/signup', AuthRoute.signUp);
 router.get('/logout', AuthRoute.getLogout);
 
