24a17362ff158e667e1b2eb44adbd2716e791fb6
khuaza9612 <102488101+kevinhuaza@users.noreply.github.com>
2022-08-28 19:15:14 -0500

    create product

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,19 +1,88 @@
-import Product from '../models/productsModel.js';
+import Products from '../models/productsModel.js';
 import AppError from '../utils/appError.js';
 import CatchAsync from '../utils/catchAsync.js';
 
 
-export const getproduct = CatchAsync(async (req, res, next) => {
-    const users = await Product.findAll();
-    if (!users) {
+export const getProducts = CatchAsync(async (req, res, next) => {
+    const products = await Products.findAll();
+    if (!products) {
       return next(new AppError('No se ha encontrado ningun producto', 400));
     }
     res.status(200).json({
       status: 'success',
-      results: users.length,
+      results: products.length,
       data: {
-        users,
+        products,
       },
     });
   });
+
+export const getProductById = CatchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const product = await Products.findByPk(id);
+    if (!id || !product) {
+      return next(new AppError('No se ha especificado el id o Producto no econtrado', 400));
+    }
+    res.status(200).json(product);
+  });
+
+
+export const deleteProduct = CatchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const products = await Products.findByPk(id);
+    if (!products || !id) {
+      return next(new AppError('No se ha especificado el id o Producto no econtrado', 400));
+    }
+    await products.destroy();
+    res.status(204).json({ message: 'Producto eliminado correctamente' });
+  });
+
+
+export const updateProduct = CatchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const { nombre, descripcion, technical_specs, precio, stock, existencia, nombre_marca, categoria, oferta, porcentaje_oferta, fotos } = req.body;
   
+    let products = await Products.findByPk(id);
+    if (!products.id || !products) {
+      return next(new AppError('No se ha especificado el id o Producto no econtrado', 400));
+    } else {
+      products = await products.update({
+        nombre, 
+        descripcion, 
+        technical_specs, 
+        precio, 
+        stock, 
+        existencia, 
+        nombre_marca, 
+        categoria,
+        oferta,
+        porcentaje_oferta,
+        fotos
+      });
+      res.status(200).json({
+        message: 'Producto actualizado correctamente',
+        products,
+      });
+    }
+  });
+
+  export const createProduct = CatchAsync(async (req, res, next) => {
+    const { nombre, descripcion, technical_specs, precio, stock, existencia, nombre_marca, categoria, oferta, porcentaje_oferta, fotos } = req.body;
+    const products = await Products.create({
+      nombre, 
+      descripcion, 
+      technical_specs, 
+      precio, 
+      stock, 
+      existencia, 
+      nombre_marca, 
+      categoria,
+      oferta,
+      porcentaje_oferta,
+      fotos
+    });
+    res.status(201).json({
+      message: 'Producto creado correctamente',
+      products,
+    });
+  } );
